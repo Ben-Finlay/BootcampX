2fdb78fce92aef4324942be94fb66f29725af030
Ben Finlay <benjamin.finlay@gmail.com>
2022-05-04 17:42:38 -0400

    Fixed broken test code in teachers.js

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -6,25 +6,8 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
-
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;
-`)
-.then(res => {
-  res.rows.forEach(row => {
-    console.log(`${row.cohort}: ${row.teacher}`);
-  })
-});
-
-
 const cohortName = process.argv[2] || 'JUL02';
-const values = [`%${cohortName}%`];
+const values = [cohortName];
 const queryString = 
 `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
@@ -36,10 +19,9 @@ WHERE cohorts.name = $1
 ORDER BY teacher;
 `;
 
-
 pool.query(queryString, values)
-  .then(res => {
-    res.rows.forEach(user => {
-      console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`);
-    })
-  });
+.then(res => {
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher}`);
+  })
+});
